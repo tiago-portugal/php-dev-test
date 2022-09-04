10562928c56c8169c2eefb249c5b6406825deff3
Tiago <57874612+tiago-portugal@users.noreply.github.com>
2022-09-04 19:14:58 +0100

    Create curl.php

---
+++ curl.php
@@ -0,0 +1,12 @@
+<?php
+$url = 'https://www.example.com';
+ 
+$curl = curl_init();
+ 
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+ 
+$data = curl_exec($curl);
+ 
+curl_close($curl);
